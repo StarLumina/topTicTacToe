2858c0aa808d0af39c56975262d4ffb2e6ac7ba4
StarLuminary <paulalexism@gmail.com>
2025-08-12 23:28:43 -0600

    Fix bug 0n diag0nal

--- script.js
+++ script.js
@@ -16,16 +16,19 @@ function Gameboard(){
   const checkForWin = (coorX,coorY) => {
     switch (true) {
     case (board[coorY][0].getValue()==board[coorY][1].getValue()&&board[coorY][1].getValue()==board[coorY][2].getValue()):
-      console.log('tomfoolery afoot')
+      console.log('hrizntal errr')
+      return true
     break
     case (board[0][coorX].getValue()==board[1][coorX].getValue()&&board[1][coorX].getValue()==board[2][coorX].getValue()):
-      console.log('tomfoolery afoot')
+      console.log('vertical errr')
+      return true
     break
-    case (board[1][1]!=null):
+    case (board[1][1].getValue()!=null):
       if (board[0][0].getValue()==board[1][1].getValue()&&board[1][1].getValue()==board[2][2].getValue()){
-        console.log('uwu')
+        console.log('et tu?')
         return true 
       } else if (board[2][0].getValue()==board[1][1].getValue()&&board[1][1].getValue()==board[0][2].getValue()){
+        console.log('brutus')
         return true 
       }
     }
@@ -47,16 +50,18 @@ function GameController(playerOne,playerTwo){
   const eventListener = document.getElementById('squares')
   eventListener.addEventListener('click', event =>{ 
     console.log(event.target.dataset.x)
-    game.playRound(event.target.dataset.x , event.target.dataset.y)
+    playRound(event.target.dataset.x , event.target.dataset.y)
   })
   const players = [
     {
       name: 'Player 1',
-      symbol: 'X'
+      symbol: 'X',
+      score:0,
     },
     {
       name: 'Player 2',
-      symbol: 'O'
+      symbol: 'O',
+      score:0,
     }
   ];
   let activePlayer = players[0]
@@ -67,10 +72,10 @@ function GameController(playerOne,playerTwo){
     console.log(`${getActivePlayer().name}'s turn`)}
   const playRound = (coorX,coorY) => {
     board.selectSquare(coorX,coorY,getActivePlayer().symbol)
-    if (board.checkForWin(coorX,coorY)) console.log('as intended')
+    if (board.checkForWin(coorX,coorY)) {activePlayer.score += 1}
     switchActivePlayer()
-    display.updateDisplay(tictic , board.getBoard().flat(Infinity))
-    printNewRound()
+    display.updateDisplay(tictic,board.getBoard().flat(Infinity))
+    display.updateScore(players)
   }
   printNewRound()
   return{playRound, getActivePlayer}
@@ -82,7 +87,13 @@ function DisplayController(){
       display[i].textContent=board[i].getValue()
     }
   }
-  return {updateDisplay}
+  const updateScore = (playerList) => {
+    let playerOneScore = document.getElementById('playerScore1')
+    let playerTwoScore = document.getElementById('playerScore2')
+    playerOneScore.textContent=playerList[0].score
+    playerTwoScore.textContent=playerList[1].score
+  }
+  return {updateDisplay, updateScore}
 }
 
 const game = GameController()
