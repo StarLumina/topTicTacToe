a047b526ffeee16edb970cdfa06ca5238f9cd0bc
StarLuminary <paulalexism@gmail.com>
2025-08-12 22:58:50 -0600

    add UI integrati0n

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function Gameboard(){
   const board = [
     [Square(),Square(),Square()],
@@ -17,11 +18,12 @@ function Gameboard(){
     case (board[coorY][0].getValue()==board[coorY][1].getValue()&&board[coorY][1].getValue()==board[coorY][2].getValue()):
       console.log('tomfoolery afoot')
     break
-    case (board[0][coorX].getValue()==board[1][coorX].getValue()&&board[1][coorX].getValue()==board[0][coorX].getValue()):
+    case (board[0][coorX].getValue()==board[1][coorX].getValue()&&board[1][coorX].getValue()==board[2][coorX].getValue()):
       console.log('tomfoolery afoot')
     break
     case (board[1][1]!=null):
       if (board[0][0].getValue()==board[1][1].getValue()&&board[1][1].getValue()==board[2][2].getValue()){
+        console.log('uwu')
         return true 
       } else if (board[2][0].getValue()==board[1][1].getValue()&&board[1][1].getValue()==board[0][2].getValue()){
         return true 
@@ -40,6 +42,13 @@ function Square(){
 
 function GameController(playerOne,playerTwo){
   const board = Gameboard()
+  const display = DisplayController()
+  const tictic = Array.from(document.getElementsByClassName('square'))
+  const eventListener = document.getElementById('squares')
+  eventListener.addEventListener('click', event =>{ 
+    console.log(event.target.dataset.x)
+    game.playRound(event.target.dataset.x , event.target.dataset.y)
+  })
   const players = [
     {
       name: 'Player 1',
@@ -59,19 +68,24 @@ function GameController(playerOne,playerTwo){
   const playRound = (coorX,coorY) => {
     board.selectSquare(coorX,coorY,getActivePlayer().symbol)
     if (board.checkForWin(coorX,coorY)) console.log('as intended')
-    // switchActivePlayer()
+    switchActivePlayer()
+    display.updateDisplay(tictic , board.getBoard().flat(Infinity))
     printNewRound()
   }
   printNewRound()
   return{playRound, getActivePlayer}
 }
 
-function displayController(){
-  
+function DisplayController(){
+  const updateDisplay = (display,board) => {
+    for (let i=0; i < 9 ;i++) {
+      display[i].textContent=board[i].getValue()
+    }
+  }
+  return {updateDisplay}
 }
 
 const game = GameController()
-game.playRound(1,1)
-game.playRound(0,0)
-game.playRound(2,2)
+
+
 
